b443e7ca9ab8e5d422cfa6fbb0cd9f277e2ea341
Ina <95830496+Inaestrela@users.noreply.github.com>
2025-12-19 18:22:00 +0000

    Import icons
    Successfully imported the icons from library on the main ts file

---
+++ main.ts
@@ -0,0 +1,20 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { importProvidersFrom } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+import { addIcons } from 'ionicons';
+import { heart, heartOutline, settings} from 'ionicons/icons';
+
+addIcons({heart, heartOutline, settings});
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(IonicStorageModule.forRoot()),
+  ],
+});
