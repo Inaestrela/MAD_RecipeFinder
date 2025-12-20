77ba25f9a0cb7c04c703e612a46fa1b4af3ee0e7
Ina <95830496+Inaestrela@users.noreply.github.com>
2025-12-20 19:10:04 +0000

    Favorites wip
    cards are display but images and tittles not showing.

--- favorites/favorites.page.ts
+++ favorites/favorites.page.ts
@@ -1,19 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { MyDataService } from '../services/my-data';
+import { IonHeader,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonToolbar, 
+  IonTitle, 
+  IonContent,
+  IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.page.html',
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonHeader,
+    IonToolbar, 
+    IonTitle, 
+    IonContent,
+    IonButton,
+    RouterLink,],
 })
-export class FavoritesPage implements OnInit {
 
-  constructor() { }
+//Load the stored favorites
+export class FavoritesPage{
+  favorites: any[] = [];
 
-  ngOnInit() {
+  constructor(private mds: MyDataService, private router: Router) { }
+
+  async ionViewDidEnter(){
+    this.favorites = (await this.mds.get('favorites')) || [];
   }
+
+  //Redirecting details button to recipe details
+  goToCard(id:number) {
+      this.router.navigate(['/recipe-details', id]);
+    }
 
 }
